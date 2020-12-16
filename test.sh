e7ef8f75a915b2ea167d6dd633861a55ac5d5b69
Charles Korpics <72992136+korpx-z@users.noreply.github.com>
2020-12-16 14:37:51 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -8,7 +8,7 @@ echo -e "\n $ANSI_YELLOW *** testing docker run - alpine *** $ANSI_RESET \n"
 
 echo -e "$ANSI_YELLOW Display current directory: $ANSI_RESET"
 docker volume create testgit
-docker run -i --rm --name some_git --mount source=testgit,target=/git quay.io/ibmz/git:latest clone https://github.com/korpx-z/hello.git
+docker run -i --rm --name some_git --mount source=testgit,target=/git quay.io/ibm/git:latest clone https://github.com/korpx-z/hello.git
 docker run --rm -i -v=testgit:/tmp/myvolume quay.io/ibm/busybox:musl cat /tmp/myvolume/hello/hello.txt
 
 
